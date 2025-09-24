75ff83755a7db843a69ee02a9f6c0194626fff41
mahes0707 <mahesw332@gmail.com>
2025-09-24 13:47:19 +0530

    initial commit

--- Javacodes/pattern1.java
+++ Javacodes/pattern1.java
@@ -1,5 +1,13 @@
 package Javacodes;
-
-public class pattern1 {
-    
+import java.util.*;
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
